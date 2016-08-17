ad5888204266e8a11024d2b731a79ceb90791f39
Nithish deva Divakar <NITHISH.divakar@gmail.com>
2016-08-17 22:12:35 +0530

    Create auto-mpg-to-hdf5.py

---
+++ Datasets/auto-mpg/auto-mpg-to-hdf5.py
@@ -0,0 +1,16 @@
+import numpy
+import h5py
+
+f = open("auto-mpg.data","r")
+D = []
+for line in f:
+    line = line.strip()
+    try:
+        data = line.split()
+        data = [float(d) for d in data[:8]]
+        D.append(data)
+	except:
+        pass
+D = numpy.asarray(D,dtype='float')
+with h5py.File('auto-mpg.hdf5', 'w') as F:
+    F.create_dataset('dataset', data=D)
