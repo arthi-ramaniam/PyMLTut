a2bea84d650e3efa2adaa3addf0c01b6ee66a93f
Nithish deva Divakar <NITHISH.divakar@gmail.com>
2016-08-19 01:01:15 +0530

    Create virtual-env setup.sh

---
+++ virtual-env setup.sh
@@ -0,0 +1,7 @@
+#cd to/your convinient/location
+
+mkdir WORK
+virtualenv ml
+source ml/bin/activate
+curl -O https://raw.githubusercontent.com/nithishdivakar/PyMLTut/master/Resources/requirements.txt
+pip install -r requirements.txt
